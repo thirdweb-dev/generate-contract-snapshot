baaf15ec6a90ecdc4e78ad87ba15342957855568
Nacho Iacovino <nacho.iacovino@gmail.com>
2022-10-28 18:49:38 +0200

    Make it work for any erc155/erc721

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-# Get owners with the quantity owned
+# Generate contract snapshot
 
-Get a JSON with the owners and the quantity of each owned item for any ERC1155 contract.
+Get a JSON with the owners and the quantity of each owned item for any ERC721 or ERC1155 contract, on any chain.
 
 ## Usage
 
-- Replace contractAddress, network and tokenId with your own
+- Install dependencies
+  
+  ```bash
+  npm install
+  # or
+  yarn
+  ```
+
+- Replace `NETWORK.ETH_MAINNET` on `alchemySettings` with the Chain you're using.
+- Replace `ethereum` on thirdweb SDK initialization with the Chain you're using. (Example: `"polygon"`, `"avalanche"`, `"fantom"` )
+- Replace the `erc` const with either `erc1155` or `erc721`, depending on your needs.
+- Replace contractAddress, network and tokenId (in case of erc1155) with the contract address to take a snapshot of.
 - Run with `npx ts-node index.ts`
 
 
--- index.ts
+++ index.ts
@@ -1,38 +1,51 @@
 // Importing libraries
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 import { createWriteStream } from 'fs';
-import 'dotenv/config';
 import { Alchemy, Network } from 'alchemy-sdk';
-import { constants } from 'ethers';
+import { BigNumberish, constants } from 'ethers';
 
-const settings = {
+const alchemySettings = {
   apiKey: 'demo', // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 
-const alchemy = new Alchemy(settings);
+const alchemy = new Alchemy(alchemySettings);
 
 // Instantiate thirdweb SDK
 const sdk = new ThirdwebSDK('ethereum');
 
+const erc: 'erc721' | 'erc1155' = 'erc1155'; // Choose erc721 or erc1155
 const contractAddress = '0xa6146896d6605c1b54af5ef861d5de2de1101646'; // Replace with your contract address
-const tokenId = '0'; // Replace with the token ID to check the balance of.
+const tokenId = '0'; // Replace with the token ID to check the balance of (in case of ERC1155, otherwise ignore)
 
-const getBalances = async () => {
+const getBalances = async (erc: "erc721" | "erc1155") => {
   const contract = await sdk.getContract(contractAddress);
-  const addresses = (
-    await alchemy.nft.getOwnersForNft(contractAddress, tokenId)
-  ).owners.filter((addr) => addr !== constants.AddressZero);
+  let addresses: string[] = [];
+  if (erc === 'erc1155') {
+    addresses = (
+      await alchemy.nft.getOwnersForNft(contractAddress, tokenId)
+    ).owners.filter((addr) => addr !== constants.AddressZero);
+  } else if (erc === 'erc721') {
+    addresses = (
+      await alchemy.nft.getOwnersForContract(contractAddress)
+    ).owners.filter((addr) => addr !== constants.AddressZero);
+  }
+
   const balances: { address: string; quantity: string }[] = [];
   const writeStream = createWriteStream('output.json');
   writeStream.write('[' + '\n');
   try {
     for (let i = 0; i < addresses.length; i++) {
       const address = addresses[i];
-      const balanceOf = await contract.erc1155.balanceOf(address, 0);
+      let balanceOf: BigNumberish = 0;
+      if (erc === 'erc1155') {
+        balanceOf = await contract.erc1155.balanceOf(address, tokenId);
+      } else if (erc === 'erc721') {
+        balanceOf = await contract.erc721.balanceOf(address);
+      }
       const objectToPush = { address, quantity: balanceOf.toString() };
 
-      if (objectToPush.quantity !== "0") {
+      if (objectToPush.quantity !== '0') {
         balances.push(objectToPush);
       }
 
@@ -52,4 +65,4 @@ const getBalances = async () => {
   return balances;
 };
 
-getBalances();
+getBalances(erc);
